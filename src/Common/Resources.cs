b8323c7818f4f0a964b8230ada58a049f58384e9
Premek Vysoky <premek.vysoky@microsoft.com>
2021-11-22 21:34:16 +0100

    Add Common project + GetResourceFile

---
+++ src/Common/Resources.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+
+namespace Common;
+
+public class Resources
+{
+    public static string GetResourceFile(string resourceFileName)
+    {
+        var assembly = Assembly.GetCallingAssembly();
+        Stream? stream = assembly.GetManifestResourceStream($"{assembly.GetName().Name}.{resourceFileName}");
+
+        if (stream == null)
+        {
+            string? resource = assembly.GetManifestResourceNames().FirstOrDefault(res => res.EndsWith(resourceFileName));
+            if (resource != null)
+            {
+                stream = assembly.GetManifestResourceStream(resource);
+            }
+        }
+
+        using (stream)
+        using (var sr = new StreamReader(stream ?? throw new FileNotFoundException($"Couldn't locate resource file '{resourceFileName}'")))
+        {
+            return sr.ReadToEnd();
+        }
+    }
+}
