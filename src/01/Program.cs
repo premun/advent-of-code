c62f2119104b272b2fbf24c0d26902278b81682d
Premek Vysoky <premek.vysoky@microsoft.com>
2021-12-02 18:15:32 +0100

    Day 1

--- src/01/Program.cs
+++ src/01/Program.cs
@@ -1,1 +1,35 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Common;
+
+var lines = Resources.GetResourceFile("input.txt").Split('\n', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+
+var lastDepth = int.MaxValue;
+var increases = 0;
+
+// Part 1
+foreach (var depth in lines.Select(int.Parse))
+{
+    if (depth > lastDepth)
+    {
+        increases++;
+    }
+
+    lastDepth = depth;
+}
+
+Console.WriteLine($"Part 1: {increases}");
+
+// Part 2
+increases = 0;
+lastDepth = int.MaxValue;
+
+var allDepths = lines.Select(int.Parse).ToArray();
+
+for (int i = 3; i < allDepths.Length; i++)
+{
+    if (allDepths[i - 3] < allDepths[i])
+    {
+        increases++;
+    }
+}
+
+Console.WriteLine($"Part 2: {increases}");
