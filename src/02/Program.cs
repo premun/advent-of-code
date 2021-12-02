4efe8bfcd6fd98c47d8bf1caa1320704f842cac0
Premek Vysoky <premek.vysoky@microsoft.com>
2021-12-02 18:36:36 +0100

    Day 2

---
+++ src/02/Program.cs
@@ -0,0 +1,59 @@
+﻿using Common;
+
+var lines = Resources.GetResourceFileLines("input.txt");
+
+var position = (0, 0);
+
+// Part 1
+foreach (var line in lines)
+{
+    var parts = line.Split(' ');
+    var direction = parts[0];
+    var distance = int.Parse(parts[1]);
+
+    switch (direction)
+    {
+        case "forward":
+            position.Item2 += distance;
+            break;
+
+        case "down":
+            position.Item1 += distance;
+            break;
+
+        case "up":
+            position.Item1 -= distance;
+            break;
+    }
+}
+
+Console.WriteLine($"Part 1: {position.Item1 * position.Item2}");
+
+// Part 2
+position = (0, 0);
+var aim = 0;
+
+foreach (var line in lines)
+{
+    var parts = line.Split(' ');
+    var direction = parts[0];
+    var distance = int.Parse(parts[1]);
+
+    switch (direction)
+    {
+        case "forward":
+            position.Item1 += aim * distance;
+            position.Item2 += distance;
+            break;
+
+        case "down":
+            aim += distance;
+            break;
+
+        case "up":
+            aim -= distance;
+            break;
+    }
+}
+
+Console.WriteLine($"Part 2: {position.Item1 * position.Item2}");
