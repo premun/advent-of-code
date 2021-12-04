95c684241e54643beec9d8b6d5c9d98528dfd7e7
Premek Vysoky <premek.vysoky@microsoft.com>
2021-12-04 16:56:21 +0100

    Day 4 Part 1

---
+++ src/04/Program.cs
@@ -0,0 +1,34 @@
+﻿using _04;
+using Common;
+
+using var inputReader = Resources.GetResourceStream("input.txt");
+
+var drawnNumbers = inputReader.ReadLine()!.Split(",").Select(int.Parse);
+
+inputReader.ReadLine();
+
+var boards = new List<Board>();
+
+while (!inputReader.EndOfStream)
+{
+    boards.Add(Board.FromStream(inputReader));
+    inputReader.ReadLine();
+}
+
+int Part1()
+{
+    foreach (var number in drawnNumbers)
+    {
+        foreach (var board in boards)
+        {
+            if (board.CrossNumber(number))
+            {
+                return number * board.GetSumOfRemainingNumbers();
+            }
+        }
+    }
+
+    throw new Exception("No winning board");
+}
+
+Console.WriteLine($"Part 1: {Part1()}");
