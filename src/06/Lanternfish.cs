3660e20eecbd5e93342a161906afc84551c6ddae
Premek Vysoky <premek.vysoky@microsoft.com>
2021-12-06 09:49:11 +0100

    Day 6 Part 1

---
+++ src/06/Lanternfish.cs
@@ -0,0 +1,23 @@
+﻿namespace _03;
+
+internal class Lanternfish
+{
+    private int _timer;
+
+    public Lanternfish(int timer)
+    {
+        _timer = timer;
+    }
+
+    public bool Tick()
+    {
+        if (_timer > 0)
+        {
+            _timer--;
+            return false;
+        }
+
+        _timer = 6;
+        return true;
+    }
+}
--- src/06/Program.cs
+++ src/06/Program.cs
@@ -1,3 +1,36 @@
-﻿using Common;
+﻿using _03;
+using Common;
 
-var lines = Resources.GetResourceFileLines("input.txt");
+var population = Resources.GetResourceFileLines("input.txt")
+    .First()
+    .Split(",", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)
+    .Select(int.Parse)
+    .Select(t => new Lanternfish(t));
+
+static int Part1(IEnumerable<Lanternfish> population, int generations)
+{
+    var thisGeneration = new Queue<Lanternfish>(population);
+    var nextGeneration = new Queue<Lanternfish>();
+
+    for (int i = 0; i < generations; i++)
+    {
+        while (thisGeneration.Count > 0)
+        {
+            var fish = thisGeneration.Dequeue();
+
+            nextGeneration.Enqueue(fish);
+
+            if (fish.Tick())
+            {
+                nextGeneration.Enqueue(new Lanternfish(8));
+            }
+        }
+
+        (thisGeneration, nextGeneration) = (nextGeneration, thisGeneration);
+    }
+
+    return thisGeneration.Count();
+}
+
+Console.WriteLine($"Part 1: {Part1(population, 80)}");
+Console.WriteLine($"Part 1: {Part1(population, 256)}");
