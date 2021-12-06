ced6e500fd28f45bf5e481caf6597d914491a2f8
Premek Vysoky <premek.vysoky@microsoft.com>
2021-12-06 16:55:54 +0100

    Day 6 Part 2

--- src/06/Program.cs
+++ src/06/Program.cs
@@ -1,36 +1,39 @@
-﻿using _03;
-using Common;
+﻿using Common;
 
 var population = Resources.GetResourceFileLines("input.txt")
     .First()
     .Split(",", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)
     .Select(int.Parse)
-    .Select(t => new Lanternfish(t));
+    .ToArray();
 
-static int Part1(IEnumerable<Lanternfish> population, int generations)
+static long[] CreateHistogram(IEnumerable<int> population)
 {
-    var thisGeneration = new Queue<Lanternfish>(population);
-    var nextGeneration = new Queue<Lanternfish>();
+    var histogram = new long[9];
 
-    for (int i = 0; i < generations; i++)
+    foreach (var i in population)
     {
-        while (thisGeneration.Count > 0)
-        {
-            var fish = thisGeneration.Dequeue();
-
-            nextGeneration.Enqueue(fish);
-
-            if (fish.Tick())
-            {
-                nextGeneration.Enqueue(new Lanternfish(8));
-            }
-        }
-
-        (thisGeneration, nextGeneration) = (nextGeneration, thisGeneration);
+        histogram[i]++;
     }
 
-    return thisGeneration.Count();
+    return histogram;
 }
 
-Console.WriteLine($"Part 1: {Part1(population, 80)}");
-Console.WriteLine($"Part 1: {Part1(population, 256)}");
+static long Evolve(long[] histogram, int generations)
+{
+    for (int i = 0; i < generations; i++)
+    {
+        var readyToBreed = histogram[0];
+        for (int j = 0; j < histogram.Length - 1; j++)
+        {
+            histogram[j] = histogram[j + 1];
+        }
+
+        histogram[8] = readyToBreed;
+        histogram[6] += readyToBreed;
+    }
+
+    return histogram.Sum();
+}
+
+Console.WriteLine($"Part 1: {Evolve(CreateHistogram(population), 80)}");
+Console.WriteLine($"Part 2: {Evolve(CreateHistogram(population), 256)}");
