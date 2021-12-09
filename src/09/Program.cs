179088d5c590fbb2123cda5d57d519b7241234d1
Premek Vysoky <premek.vysoky@microsoft.com>
2021-12-09 14:22:03 +0100

    Day 9

--- src/09/Program.cs
+++ src/09/Program.cs
@@ -1,3 +1,52 @@
-﻿using Common;
+﻿using _09;
+using Common;
 
-var lines = Resources.GetResourceFileLines("input.txt");
+var heatMap = Resources.GetResourceFileLines("input.txt")
+    .Select(line => line.Select(n => n - '0').ToArray())
+    .ToArray();
+
+static long Part1(int[][] heatMap)
+{
+    long result = 0;
+
+    for (int row = 0; row < heatMap.Length; row++)
+    {
+        for (int column = 0; column < heatMap[row].Length; column++)
+        {
+            int current = heatMap[row][column];
+
+            if (row > 0 && heatMap[row - 1][column] <= current)
+            {
+                continue;
+            }
+
+            if (column > 0 && heatMap[row][column - 1] <= current)
+            {
+                continue;
+            }
+
+            if (column < heatMap[row].Length - 1 && heatMap[row][column + 1] <= current)
+            {
+                continue;
+            }
+
+            if (row < heatMap.Length - 1 && heatMap[row + 1][column] <= current)
+            {
+                continue;
+            }
+
+            result += 1 + current;
+        }
+    }
+
+    return result;
+}
+
+static long Part2(int[][] heatMap)
+{
+    return new BasinMap(heatMap).FindBasins().Select(b => (long)b.Size).OrderByDescending(b => b).Take(3).Aggregate((acc, b) => acc * b);
+}
+
+
+Console.WriteLine($"Part 1: {Part1(heatMap)}");
+Console.WriteLine($"Part 2: {Part2(heatMap)}");
