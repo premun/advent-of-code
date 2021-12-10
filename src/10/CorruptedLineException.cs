e13a0c23118402faffc552e4fadcc82feda8411a
Premek Vysoky <premek.vysoky@microsoft.com>
2021-12-10 15:01:13 +0100

    Clean up Day 10

---
+++ src/10/CorruptedLineException.cs
@@ -0,0 +1,11 @@
+﻿namespace _10;
+
+class CorruptedLineException : Exception
+{
+    public CorruptedLineException(char c)
+    {
+        CorruptedChar = c;
+    }
+
+    public char CorruptedChar { get; }
+}
