0dbfafb90fcd4a63659065d8b58ef642fb0a53c7
Premek Vysoky <premek.vysoky@microsoft.com>
2021-12-10 17:32:35 +0100

    Clean up Day 5

---
+++ src/05/Coor.cs
@@ -0,0 +1,7 @@
+﻿namespace _06;
+
+record Coor(int X, int Y)
+{
+    public static Coor operator -(Coor me, Coor other) => new(me.X - other.X, me.Y - other.Y);
+    public static Coor operator +(Coor me, Coor other) => new(me.X + other.X, me.Y + other.Y);
+}
