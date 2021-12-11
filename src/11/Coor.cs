6b175174ddcfbeb48a70a03fbdc4a30e72008a19
Premek Vysoky <premek.vysoky@microsoft.com>
2021-12-11 12:37:07 +0100

    Day 11

---
+++ src/11/Coor.cs
@@ -0,0 +1,7 @@
+﻿namespace _11;
+
+record Coor(int Row, int Column)
+{
+    public static Coor operator -(Coor me, Coor other) => new(me.Row - other.Row, me.Column - other.Column);
+    public static Coor operator +(Coor me, Coor other) => new(me.Row + other.Row, me.Column + other.Column);
+}
--- src/11/Program.cs
+++ src/11/Program.cs
@@ -1,3 +1,102 @@
-﻿using Common;
+﻿using _11;
+using Common;
 
-var lines = Resources.GetResourceFileLines("input.txt");
+var map = Resources.GetResourceFileLines("input.txt")
+    .Select(line => line.Select(n => n - '0').ToArray())
+    .ToArray();
+
+static int Step(int[][] map)
+{
+    var flashingOctos = new Queue<Coor>();
+    var directions = new Coor[]
+    {
+        new(-1, -1),
+        new(-1, 0),
+        new(-1, 1),
+        new(0, -1),
+        new(0, 1),
+        new(1, -1),
+        new(1, 0),
+        new(1, 1),
+    };
+
+    // First increase all by 1
+    for (int row = 0; row < map.Length; row++)
+    {
+        for (int column = 0; column < map[row].Length; column++)
+        {
+            if (++map[row][column] == 10)
+            {
+                flashingOctos.Enqueue(new Coor(row, column));
+            }
+        }
+    }
+
+    // Flash
+    var flashedOctos = new Queue<Coor>();
+    while (flashingOctos.TryDequeue(out var flashing))
+    {
+        flashedOctos.Enqueue(flashing);
+
+        foreach (var direction in directions)
+        {
+            var around = flashing + direction;
+
+            // Out of bounds
+            if (around.Row < 0 || around.Column < 0 || around.Row >= map.Length || around.Column >= map[0].Length)
+            {
+                continue;
+            }
+
+            if (++map[around.Row][around.Column] == 10)
+            {
+                flashingOctos.Enqueue(new Coor(around.Row, around.Column));
+            }
+        }
+    }
+
+    var flashes = flashedOctos.Count;
+
+    // Reset flashed to 0
+    while (flashedOctos.TryDequeue(out var flashed))
+    {
+        map[flashed.Row][flashed.Column] = 0;
+    }
+
+    return flashes;
+}
+
+static long Part1(int[][] map)
+{
+    long flashes = 0;
+
+    for (int i = 0; i < 100; i++)
+    {
+        flashes += Step(map);
+    }
+
+    return flashes;
+}
+
+static int Part2(int[][] map)
+{
+    int step = 0;
+
+    while (true)
+    {
+        step++;
+
+        if (Step(map) == map.Length * map[0].Length)
+        {
+            return step;
+        }
+    }
+}
+
+Console.WriteLine($"Part 1: {Part1(map)}");
+
+map = Resources.GetResourceFileLines("input.txt")
+    .Select(line => line.Select(n => n - '0').ToArray())
+    .ToArray();
+
+Console.WriteLine($"Part 2: {Part2(map)}");
