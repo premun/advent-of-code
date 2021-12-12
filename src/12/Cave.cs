653f2f504b5e216f234995ac0ad73e6bc65aba16
Premek Vysoky <premek.vysoky@microsoft.com>
2021-12-12 18:35:01 +0100

    Day 12 Part 1

---
+++ src/12/Cave.cs
@@ -0,0 +1,12 @@
+﻿namespace _12;
+
+class Cave
+{
+    public string Name { get; set; }
+
+    public bool IsSmall => Name[0] >= 'a' && Name[0] <= 'z';
+
+    public List<Cave> Paths = new();
+
+    public Cave(string name) => Name = name;
+}
