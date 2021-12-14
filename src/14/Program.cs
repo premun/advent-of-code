ff2e683629b7783e14663e930d42b62049afda75
Premek Vysoky <premek.vysoky@microsoft.com>
2021-12-14 19:29:47 +0100

    Day 14 Part 2

--- src/14/Program.cs
+++ src/14/Program.cs
@@ -8,53 +8,21 @@ Dictionary<(char, char), char> rules = lines
     .Select(line => line.Split(" -> "))
     .ToDictionary(l => (l[0][0], l[0][1]), l => l[1][0]);
 
-static void AddOrBump<TKey>(Dictionary<TKey, long> dict, TKey key) where TKey : notnull
+static void AddOrBump<TKey>(Dictionary<TKey, long> dict, TKey key, long amount) where TKey : notnull
 {
     if (dict.ContainsKey(key))
     {
-        dict[key]++;
+        dict[key] += amount;
     }
     else
     {
-        dict.Add(key, 1);
+        dict.Add(key, amount);
     }
 }
 
-static long Evolve(string template, Dictionary<(char, char), char> rules, int steps)
+static long[] GetFrequencies(Dictionary<(char, char), long> polymers)
 {
-    var polymers = new Dictionary<(char, char), long>();
-
-    for (int i = 1; i < template.Length; i++)
-    {
-        var key = (template[i - 1], template[i]);
-        AddOrBump(polymers, key);
-    }
-
-    for (int i = 0; i < steps; i++)
-    {
-        foreach (var pair in polymers.ToList())
-        {
-            var key = pair.Key;
-            var count = pair.Value;
-
-            if (!rules.ContainsKey(key) || count == 0)
-            {
-                continue;
-            }
-
-            polymers[key]--;
-
-            var newPolymer = rules[key];
-
-            AddOrBump(polymers, (key.Item1, newPolymer));
-            AddOrBump(polymers, (newPolymer, key.Item2));
-        }
-    }
-
     var frequencies = new long['Z' - 'A' + 1];
-
-    frequencies[template.First() - 'A']++;
-    frequencies[template.Last() - 'A']++;
 
     foreach (var pair in polymers)
     {
@@ -65,8 +33,59 @@ static long Evolve(string template, Dictionary<(char, char), char> rules, int st
         frequencies[polymer2 - 'A'] += pair.Value;
     }
 
-    return (frequencies.Max() - frequencies.Min()) / 2;
+    return frequencies;
 }
 
-Console.WriteLine($"Part 1: {Evolve(template, rules, 4)}");
-// Console.WriteLine($"Part 2: {Evolve(template, rules, 40)}");
+void Evolve(Dictionary<(char, char), long> polymers)
+{
+    foreach (var pair in polymers.ToList())
+    {
+        var key = pair.Key;
+        var count = pair.Value;
+
+        if (count == 0)
+        {
+            continue;
+        }
+
+        if (!rules.ContainsKey(key))
+        {
+            continue;
+        }
+
+        polymers[key] -= count;
+
+        var newPolymer = rules[key];
+        AddOrBump(polymers, (key.Item1, newPolymer), count);
+        AddOrBump(polymers, (newPolymer, key.Item2), count);
+    }
+}
+
+long EvolveAndCount(int steps)
+{
+    // Read
+    var polymers = new Dictionary<(char, char), long>();
+    for (int i = 1; i < template.Length; i++)
+    {
+        AddOrBump(polymers, (template[i - 1], template[i]), 1);
+    }
+
+    // Evolve
+    for (int i = 0; i < steps; i++)
+    {
+        Evolve(polymers);
+    }
+
+    // Count
+    var frequencies = GetFrequencies(polymers);
+
+    // Edges are accounted for just once, the rest twice because of overlaps
+    // e.g. ABCD turns into (A,B) (B,C) (C,D)
+    frequencies[template.First() - 'A']++;
+    frequencies[template.Last() - 'A']++;
+
+    return (frequencies.Max() - frequencies.Where(f => f > 0).Min()) / 2;
+}
+
+Console.WriteLine($"Part 1: {EvolveAndCount(10)}");
+Console.WriteLine($"Part 2: {EvolveAndCount(40)}");
