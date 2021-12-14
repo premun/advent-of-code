39416f3a06ca90ef5435a49a75af73383f5bc637
Premek Vysoky <premek.vysoky@microsoft.com>
2021-12-14 16:27:53 +0100

    Day 14 - new algorithm

--- src/14/Program.cs
+++ src/14/Program.cs
@@ -1,55 +1,72 @@
-﻿using System.Text;
-using Common;
+﻿using Common;
 
 var lines = Resources.GetResourceFileLines("input.txt");
 
 string template = lines.First();
-Dictionary<string, string> rules = lines.Skip(1).Select(line => line.Split(" -> ")).ToDictionary(l => l[0], l => l[1]);
+Dictionary<(char, char), char> rules = lines
+    .Skip(1)
+    .Select(line => line.Split(" -> "))
+    .ToDictionary(l => (l[0][0], l[0][1]), l => l[1][0]);
 
-static string InsertPolymers(string template, Dictionary<string, string> rules)
+static void AddOrBump<TKey>(Dictionary<TKey, long> dict, TKey key) where TKey : notnull
 {
-    var result = new StringBuilder();
+    if (dict.ContainsKey(key))
+    {
+        dict[key]++;
+    }
+    else
+    {
+        dict.Add(key, 1);
+    }
+}
+
+static long Evolve(string template, Dictionary<(char, char), char> rules, int steps)
+{
+    var polymers = new Dictionary<(char, char), long>();
 
     for (int i = 1; i < template.Length; i++)
     {
-        result.Append(template[i - 1]);
-
-        if (rules.TryGetValue(template[i - 1] + "" + template[i], out var inserted))
-        {
-            result.Append(inserted);
-        }
+        var key = (template[i - 1], template[i]);
+        AddOrBump(polymers, key);
     }
 
-    result.Append(template.Last());
-
-    return result.ToString();
-}
-
-static long Evolve(string template, Dictionary<string, string> rules, int steps)
-{
     for (int i = 0; i < steps; i++)
     {
-        Console.Write("\r{0}/{1}\t", i + 1, steps);
-        template = InsertPolymers(template, rules);
+        foreach (var pair in polymers.ToList())
+        {
+            var key = pair.Key;
+            var count = pair.Value;
+
+            if (!rules.ContainsKey(key) || count == 0)
+            {
+                continue;
+            }
+
+            polymers[key]--;
+
+            var newPolymer = rules[key];
+
+            AddOrBump(polymers, (key.Item1, newPolymer));
+            AddOrBump(polymers, (newPolymer, key.Item2));
+        }
     }
 
-    var frequencies = new Dictionary<char, long>();
+    var frequencies = new long['Z' - 'A' + 1];
 
-    for (int i = 0; i < template.Length; i++)
+    frequencies[template.First() - 'A']++;
+    frequencies[template.Last() - 'A']++;
+
+    foreach (var pair in polymers)
     {
-        if (frequencies.ContainsKey(template[i]))
-        {
-            frequencies[template[i]]++;
-        }
-        else
-        {
-            frequencies.Add(template[i], 1);
-        }
+        var polymer1 = pair.Key.Item1;
+        var polymer2 = pair.Key.Item2;
+
+        frequencies[polymer1 - 'A'] += pair.Value;
+        frequencies[polymer2 - 'A'] += pair.Value;
     }
 
-    var f = frequencies.Select(p => p.Value);
-    return f.Max() - f.Min();
+    return (frequencies.Max() - frequencies.Min()) / 2;
 }
 
-Console.WriteLine($"Part 1: {Evolve(template, rules, 10)}");
-Console.WriteLine($"Part 2: {Evolve(template, rules, 40)}");
+Console.WriteLine($"Part 1: {Evolve(template, rules, 4)}");
+// Console.WriteLine($"Part 2: {Evolve(template, rules, 40)}");
