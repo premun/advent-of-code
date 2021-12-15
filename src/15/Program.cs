95aba6d5750bcd6980d58f4be9b5fa1f4e3b8164
Premek Vysoky <premek.vysoky@microsoft.com>
2021-12-15 20:53:02 +0100

    Make it Dijkstra

--- src/15/Program.cs
+++ src/15/Program.cs
@@ -5,6 +5,36 @@ var map = Resources.GetResourceFileLines("input.txt")
     .Select(line => line.Select(c => c - '0').ToArray())
     .ToArray();
 
-var pathFinder = new PathFinder(map);
+static int[][] Resize(int[][] map, int factor)
+{
+    int sizeY = map.Length;
+    int sizeX = map[0].Length;
 
-Console.WriteLine($"Part 1: {pathFinder.Search()}");
+    var resizedMap = new int[sizeY * factor][];
+
+    for (int y = 0; y < sizeY * factor; y++)
+    {
+        resizedMap[y] = new int[sizeX * factor];
+
+        for (int x = 0; x < sizeX * factor; x++)
+        {
+            int original = map[y % sizeY][x % sizeX];
+            int increase = (x / sizeY) + (y / sizeX);
+
+            resizedMap[y][x] = original + increase;
+
+            while (resizedMap[y][x] > 9)
+            {
+                resizedMap[y][x] -= 9;
+            }
+        }
+    }
+
+    return resizedMap;
+}
+
+var pathFinder = new PathFinder(map);
+Console.WriteLine($"Part 1: {pathFinder.FindShortestPath()}");
+
+pathFinder = new PathFinder(Resize(map, 5));
+Console.WriteLine($"Part 2: {pathFinder.FindShortestPath()}");
