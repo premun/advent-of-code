d694232bc7895032d1417a1486102a6c17ed8234
Premek Vysoky <premek.vysoky@microsoft.com>
2021-12-15 12:10:40 +0100

    Day 15 Part 1

--- src/15/Program.cs
+++ src/15/Program.cs
@@ -1,3 +1,10 @@
-﻿using Common;
+﻿using _15;
+using Common;
 
-var lines = Resources.GetResourceFileLines("input.txt");
+var map = Resources.GetResourceFileLines("input.txt")
+    .Select(line => line.Select(c => c - '0').ToArray())
+    .ToArray();
+
+var pathFinder = new PathFinder(map);
+
+Console.WriteLine($"Part 1: {pathFinder.Search()}");
