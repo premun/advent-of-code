08b193cb05b0be7b899b3462e355dab492c39193
Premek Vysoky <premek.vysoky@microsoft.com>
2021-12-16 23:10:19 +0100

    Day 16 Part 1

--- src/16/Program.cs
+++ src/16/Program.cs
@@ -1,3 +1,8 @@
-﻿using Common;
+﻿using _16;
+using Common;
 
-var lines = Resources.GetResourceFileLines("input.txt");
+var input = "38006F45291200";// Resources.GetResourceFileLines("input.txt").First();
+
+var packet = Packet.FromHexadecimal(input);
+
+Console.WriteLine((packet as LiteralPacket)!.Value);
