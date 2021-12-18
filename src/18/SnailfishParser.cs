daae97e07c43626982b3b80137b8b38d17ac82e7
Premek Vysoky <premek.vysoky@microsoft.com>
2021-12-18 16:28:56 +0100

    Snailfish parser + find exploding

---
+++ src/18/SnailfishParser.cs
@@ -0,0 +1,55 @@
+﻿namespace _18;
+
+class SnailfishParser
+{
+    private readonly Stack<SnailfishNumber> _stack = new();
+
+    public SnailfishNumber Parse(string input)
+    {
+        var reader = new TokenReader(input);
+        _stack.Clear();
+
+        while (true)
+        {
+            Token token = reader.GetNext(out var literal);
+
+            switch (token)
+            {
+                case Token.Start:
+                case Token.Delimeter:
+                    break;
+
+                case Token.Literal:
+                    _stack.Push(new Literal(literal));
+                    break;
+
+                case Token.End:
+                    var right = _stack.Pop();
+                    var left = _stack.Pop();
+                    var newNumber = new Pair(Right: right, Left: left);
+
+                    left.Parent = newNumber;
+                    right.Parent = newNumber;
+
+                    newNumber.Reduce();
+
+                    _stack.Push(newNumber);
+
+                    break;
+
+                case Token.EndOfStream:
+                    if (_stack.Count != 1)
+                    {
+                        throw new InvalidOperationException($"Invalid count ({_stack.Count}) of token at the EOF!");
+                    }
+
+                    return _stack.Pop();
+
+                default:
+                    throw new Exception("???");
+            }
+        }
+
+        throw new Exception("???");
+    }
+}
---
+++ src/18/TokenReader.cs
@@ -0,0 +1,59 @@
+﻿namespace _18;
+
+enum Token
+{
+    Start,      // [
+    Literal,    // number
+    End,        // ]
+    Delimeter,  // ,
+    EndOfStream,
+}
+
+class TokenReader
+{
+    private readonly string _source;
+
+    public int Position { get; private set; } = 0;
+
+    public TokenReader(string source)
+    {
+        _source = source;
+    }
+
+    public Token GetNext(out long literalValue)
+    {
+        literalValue = 0;
+
+        if (Position == _source.Length)
+        {
+            return Token.EndOfStream;
+        }
+
+        switch (Current)
+        {
+            case '[':
+                Position++;
+                return Token.Start;
+
+            case ']':
+                Position++;
+                return Token.End;
+
+            case ',':
+                Position++;
+                return Token.Delimeter;
+
+            default:
+                while (Position < _source.Length && Current >= '0' && Current <= '9')
+                {
+                    literalValue *= 10;
+                    literalValue += Current - '0';
+                    Position++;
+                }
+
+                return Token.Literal;
+        }
+    }
+
+    private char Current => _source[Position];
+}
