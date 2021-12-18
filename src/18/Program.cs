3f5264bf9451e9cd6688883e922ba8b10251a611
Premek Vysoky <premek.vysoky@microsoft.com>
2021-12-18 16:28:56 +0100

    Snailfish parser + find exploding

--- src/18/Program.cs
+++ src/18/Program.cs
@@ -1,3 +1,9 @@
-﻿using Common;
+﻿using _18;
+using Common;
 
-var lines = Resources.GetResourceFileLines("input.txt");
+var parser = new SnailfishParser();
+// var numbers = Resources.GetResourceFileLines("input.txt").Select(l => parser.Parse(l));
+
+var number = parser.Parse("[[[[[9,8],1],2],3],4]");
+
+Console.WriteLine();
---
+++ src/18/SnailfishNumber.cs
@@ -0,0 +1,46 @@
+﻿namespace _18;
+
+abstract record SnailfishNumber
+{
+    /* abstract long Magnitude { get; } */
+
+    public Pair? Parent { get; set; }
+
+    public abstract bool TryExplode(int depth);
+
+    protected bool IsLeftChild() => Parent?.Left == this;
+
+    protected bool IsRightChild() => Parent?.Right == this;
+}
+
+record Literal(long Value) : SnailfishNumber
+{
+    public override bool TryExplode(int depth) => false;
+}
+
+record Pair(SnailfishNumber Left, SnailfishNumber Right) : SnailfishNumber
+{
+    public void Reduce()
+    {
+        // Explode
+        TryExplode(0);
+
+        // Check for 10+
+    }
+
+    public override bool TryExplode(int depth)
+    {
+        if (depth == 4)
+        {
+            if (Left is not Literal left || Right is not Literal right)
+            {
+                throw new Exception("Depth 5 detected!");
+            }
+
+            Console.WriteLine($"Exploding [{left.Value}, {right.Value}]");
+            return true;
+        }
+
+        return Left.TryExplode(depth + 1) || Right.TryExplode(depth + 1);
+    }
+}
