f1fc13c89979890be864b57ee55916dfda0327c3
Premek Vysoky <premek.vysoky@microsoft.com>
2021-12-20 12:36:30 +0100

    Day 20 Part 1

--- src/20/Program.cs
+++ src/20/Program.cs
@@ -1,3 +1,25 @@
-﻿using Common;
+﻿using _20;
+using Common;
 
 var lines = Resources.GetResourceFileLines("input.txt");
+var enhancementAlgorithm = Array.AsReadOnly(lines.First().Select(c => c == '#').ToArray());
+
+var image = lines.Skip(1).Select(line => line.Select(c => c == '#').ToArray()).ToArray();
+
+var enhancer = new ImageEnhancer(enhancementAlgorithm);
+
+for (int i = 1; i <= 50; i++)
+{
+    image = enhancer.Enhance(image);
+
+    if (i == 2)
+    {
+        Console.WriteLine($"Part 1: {image.SelectMany(row => row).Count(pixel => pixel)}");
+    }
+}
+
+Console.WriteLine($"Part 2: {image.SelectMany(row => row).Count(pixel => pixel)}");
+
+Console.WriteLine();
+
+ImageEnhancer.Display(image);
