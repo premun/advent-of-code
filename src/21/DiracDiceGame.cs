48a128e5f434f7f2916bae4593abef8d863f9ae5
Premek Vysoky <premek.vysoky@microsoft.com>
2021-12-21 09:36:36 +0100

    Day 21 Part 1

---
+++ src/21/DiracDiceGame.cs
@@ -0,0 +1,56 @@
+﻿namespace _21;
+
+class Player
+{
+    public int Position { get; set; }
+    public int Points { get; set; }
+}
+
+class DiracDiceGame
+{
+    private readonly IDice _dice;
+    private readonly int _maxPosition;
+    private readonly int _winningPoints;
+    private readonly Player[] _players;
+    private int _currentPlayer = 0;
+
+    public DiracDiceGame(IDice dice, int[] startingPosition, int maxPosition = 10, int winningPoints = 1000)
+    {
+        _dice = dice;
+        _maxPosition = maxPosition;
+        _winningPoints = winningPoints;
+        _players = startingPosition
+            .Select(position => new Player()
+            {
+                Position = position - 1,
+                Points = 0,
+            })
+            .ToArray();
+    }
+
+    public Player[] RunGame()
+    {
+        Player player;
+
+        do
+        {
+            player = _players[_currentPlayer];
+
+            var points = _dice.Throw() + _dice.Throw() + _dice.Throw();
+            player.Position += points;
+            player.Position %= _maxPosition;
+            player.Points += player.Position + 1;
+            _currentPlayer++;
+            _currentPlayer %= _players.Length;
+
+        } while (player.Points < _winningPoints);
+
+        return _players
+            .Select(p => new Player
+            {
+                Position = p.Position == _maxPosition ? 1 : p.Position + 1,
+                Points = p.Points,
+            })
+            .ToArray();
+    }
+}
--- src/21/Program.cs
+++ src/21/Program.cs
@@ -1,3 +1,14 @@
-﻿using Common;
+﻿using _21;
+using Common;
 
-var lines = Resources.GetResourceFileLines("input.txt");
+var startingPositions = Resources.GetResourceFileLines("input.txt")
+    .Select(line => line.Split(" ").Last())
+    .Select(int.Parse)
+    .ToArray();
+
+var dice = new DeterministicDice();
+var game = new DiracDiceGame(dice, startingPositions, 10, 1000);
+
+var players = game.RunGame();
+
+Console.WriteLine($"Part 1: {dice.Throws * players.Select(p => p.Points).Min()}");
