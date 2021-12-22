66f232a7357fbc6d870c2edbbf3b88d0b1e4d4d8
Premek Vysoky <premek.vysoky@microsoft.com>
2021-12-22 17:27:32 +0100

    Add tests

--- src/21/Program.cs
+++ src/21/Program.cs
@@ -12,19 +12,29 @@ var players = new SimpleDiracDiceGame(dice, maxPosition: 10, winningPoints: 1000
 
 Console.WriteLine($"Part 1: {dice.Throws * players.Select(p => p.Points).Min()}");
 
-foreach (var points in new[] { 1, 2, 3, 21 })
+var testData = new[]
 {
-    var quantumGame = new QuantumDiracDiceGame(maxPosition: 10, winningPoints: points, diceSides: 3);
+    (1, 27, 0 ),
+    (2, 183, 156 ),
+    (3, 990, 207 ),
+    (10, 18973591, 12657100 ),
+    (21, 444356092776315, 341960390180808 ),
+};
+
+foreach (var data in testData)
+{
+    var quantumGame = new QuantumDiracDiceGame(maxPosition: 10, winningPoints: data.Item1, diceSides: 3);
     var possibleWins1 = quantumGame.RunGame(startingPositions[0], startingPositions[1], true);
     var possibleWins2 = quantumGame.RunGame(startingPositions[1], startingPositions[0], false);
 
-    Console.WriteLine($"Part 2:   {possibleWins1,15} vs {possibleWins2,15}");
+    var def = Console.ForegroundColor;
+    Console.ForegroundColor = data.Item2 == possibleWins1 ? ConsoleColor.Green : ConsoleColor.Red;
+    Console.Write($"{possibleWins1,17}");
+    Console.ForegroundColor = def;
+    Console.Write($" vs ");
+    Console.ForegroundColor = data.Item3 == possibleWins2 ? ConsoleColor.Green : ConsoleColor.Red;
+    Console.WriteLine($"{possibleWins2,15}");
+    Console.ForegroundColor = ConsoleColor.Gray;
+    Console.WriteLine($"{data.Item2,17}    {data.Item3,15}");
+    Console.ForegroundColor = def;
 }
-
-Console.WriteLine($"Expected: {27,15} vs {0,15}");
-Console.WriteLine($"Expected: {183,15} vs {156,15}");
-Console.WriteLine($"Expected: {990,15} vs {207,15}");
-Console.WriteLine("Expected: 444356092776315 vs 341960390180808");
-
-// 11666337147
-// 444356092776315
