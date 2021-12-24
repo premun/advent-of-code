4258434b8ee6b8282ac56de1cb2c1e21e7919306
Premek Vysoky <premek.vysoky@microsoft.com>
2021-12-24 18:15:39 +0100

    Should work but does not

--- src/23/Program.cs
+++ src/23/Program.cs
@@ -10,4 +10,54 @@ var world = new AmphipodWorld(chars);
 
 Console.WriteLine(world);
 
-//Console.WriteLine($"Part 1: ");
+
+
+static int Part1(AmphipodWorld world, IEnumerable<int> previousWorlds, int energyCost, int bestSolution = int.MaxValue)
+{
+    if (energyCost >= bestSolution)
+    {
+        return int.MaxValue;
+    }
+
+    if (world.IsFinished())
+    {
+        //Console.WriteLine($"Finished at {energyCost}");
+        //Console.WriteLine();
+        //Console.WriteLine(world);
+        //Console.ReadLine();
+        return energyCost;
+    }
+
+    foreach (var move in world.GetPossibleMoves())
+    {
+        var hash = move.World.GetHashCode();
+        if (previousWorlds.Contains(hash))
+        {
+            continue;
+        }
+
+        if (energyCost + move.EnergyCost > bestSolution)
+        {
+            continue;
+        }
+
+        //Console.Clear();
+        //Console.WriteLine(new string('>', previousWorlds.Count()));
+        //Console.WriteLine(energyCost + move.EnergyCost);
+        //Console.WriteLine(move.World);
+        //Thread.Sleep(300);
+        //Console.ReadLine();
+
+        var newSolution = Part1(move.World, previousWorlds.Append(hash), energyCost + move.EnergyCost, bestSolution);
+
+        if (newSolution < bestSolution)
+        {
+            bestSolution = newSolution;
+            Console.Write($"\r{bestSolution}         {previousWorlds.Count()}          ");
+        }
+    }
+
+    return bestSolution;
+}
+
+Console.WriteLine($"Part 1: {Part1(world, new[] { world.GetHashCode() }, 0)}");
