36010db6873323b9919a5ca8d39215b2e0c5e79e
Premek Vysoky <premek.vysoky@microsoft.com>
2021-12-26 18:56:28 +0100

    Day 23 Part 1

--- src/23/Program.cs
+++ src/23/Program.cs
@@ -1,63 +1,73 @@
 ﻿using _23;
 using Common;
 
-var chars = Resources.GetResourceFile("input.txt")
-    .Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries)
-    .Select(line => line.Select(field => field).ToArray())
-    .ToArray();
+var world = new AmphipodWorld(Resources.GetResourceFile("input.txt"));
 
-var world = new AmphipodWorld(chars);
+var finishedWorld = new AmphipodWorld(
+    "#############" + Environment.NewLine +
+    "#...........#" + Environment.NewLine +
+    "###A#B#C#D###" + Environment.NewLine +
+    "  #A#B#C#D#" + Environment.NewLine +
+    "  #########");
 
-Console.WriteLine(world);
-
-
-
-static int Part1(AmphipodWorld world, IEnumerable<int> previousWorlds, int energyCost, int bestSolution = int.MaxValue)
+static int Part1(AmphipodWorld startWorld, AmphipodWorld finishedWorld)
 {
-    if (energyCost >= bestSolution)
+    // Dijkstra
+    var queue = new PriorityQueue<AmphipodWorld, int>();
+    queue.Enqueue(startWorld, 0);
+
+    var lowestEnergies = new Dictionary<int, int>
     {
-        return int.MaxValue;
-    }
+        [startWorld.GetHashCode()] = 0
+    };
 
-    if (world.IsFinished())
+    var prev = new Dictionary<int, (AmphipodWorld, int)>();
+
+    while (queue.Count > 0)
     {
-        //Console.WriteLine($"Finished at {energyCost}");
-        //Console.WriteLine();
-        //Console.WriteLine(world);
-        //Console.ReadLine();
-        return energyCost;
-    }
+        var current = queue.Dequeue();
+        var hash = current.GetHashCode();
+        var currentEnergy = lowestEnergies[hash];
 
-    foreach (var move in world.GetPossibleMoves())
-    {
-        var hash = move.World.GetHashCode();
-        if (previousWorlds.Contains(hash))
+        foreach (var move in current.GetPossibleMoves())
         {
-            continue;
-        }
+            var hash2 = move.World.GetHashCode();
+            if (!lowestEnergies.TryGetValue(hash2, out var neighbourEnergy))
+            {
+                neighbourEnergy = int.MaxValue;
+            }
 
-        if (energyCost + move.EnergyCost > bestSolution)
-        {
-            continue;
-        }
-
-        //Console.Clear();
-        //Console.WriteLine(new string('>', previousWorlds.Count()));
-        //Console.WriteLine(energyCost + move.EnergyCost);
-        //Console.WriteLine(move.World);
-        //Thread.Sleep(300);
-        //Console.ReadLine();
-
-        var newSolution = Part1(move.World, previousWorlds.Append(hash), energyCost + move.EnergyCost, bestSolution);
-
-        if (newSolution < bestSolution)
-        {
-            bestSolution = newSolution;
-            Console.Write($"\r{bestSolution}         {previousWorlds.Count()}          ");
+            var newEnergy = currentEnergy + move.EnergyCost;
+            if (newEnergy < neighbourEnergy)
+            {
+                lowestEnergies[hash2] = newEnergy;
+                queue.Enqueue(move.World, newEnergy);
+                prev[hash2] = (current, currentEnergy);
+            }
         }
     }
 
-    return bestSolution;
+    var start = startWorld.GetHashCode();
+    var currentHash = finishedWorld.GetHashCode();
+    var moves = new List<(AmphipodWorld, int)>();
+    while (currentHash != start)
+    {
+        moves.Add(prev[currentHash]);
+        currentHash = prev[currentHash].Item1.GetHashCode();
+    }
+
+    // Print the sequence
+    int i = 1;
+    foreach (var w in moves.Reverse<(AmphipodWorld, int)>())
+    {
+        Console.WriteLine($"{i++}# - {w.Item2}:");
+        Console.WriteLine(w.Item1);
+    }
+
+    Console.WriteLine(lowestEnergies[finishedWorld.GetHashCode()]);
+    Console.WriteLine(finishedWorld);
+
+    return lowestEnergies[finishedWorld.GetHashCode()];
 }
 
-Console.WriteLine($"Part 1: {Part1(world, new[] { world.GetHashCode() }, 0)}");
+Console.WriteLine($"Part 1: {Part1(world, finishedWorld)}");
