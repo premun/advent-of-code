a364609f2eee516a6700e834cc6f5fac6dcaf779
Premek Vysoky <premek.vysoky@microsoft.com>
2022-01-10 09:53:48 +0100

    2020 Day 1

--- src/2020/01/Program.cs
+++ src/2020/01/Program.cs
@@ -1,3 +1,26 @@
 ﻿using AdventOfCode.Common;
 
-var lines = Resources.GetResourceFileLines("input.txt");
+var numbers = Resources.GetResourceFile("input.txt")
+    .Split(Environment.NewLine)
+    .Where(s => !string.IsNullOrEmpty(s))
+    .Select(x => long.Parse(x));
+
+// Part 1
+var part1 = numbers.First(n => numbers.Any(m => m + n == 2020));
+Console.WriteLine(part1 * (2020 - part1));
+
+// Part 2
+foreach (var n in numbers)
+{
+    foreach (var m in numbers)
+    {
+        foreach (var o in numbers)
+        {
+            if (n + m + o == 2020)
+            {
+                Console.WriteLine(n * m * o);
+                return;
+            }
+        }
+    }
+}
