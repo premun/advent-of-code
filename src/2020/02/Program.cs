a714be104c1abf8736a6acbd3ce7733e0ea4cb7b
Premek Vysoky <premek.vysoky@microsoft.com>
2022-01-10 10:24:39 +0100

    2020 Day 2 and 3

--- src/2020/02/Program.cs
+++ src/2020/02/Program.cs
@@ -1,3 +1,30 @@
 ﻿using AdventOfCode.Common;
 
 var lines = Resources.GetResourceFileLines("input.txt");
+
+static bool IsValidPassword(string definition)
+{
+    var parts = definition.Split(' ');
+    var limits = parts[0].Split('-');
+    var min = int.Parse(limits[0]);
+    var max = int.Parse(limits[1]);
+
+    var c = parts[1][0];
+    var occurences = parts[2].Count(x => c == x);
+
+    return occurences >= min && occurences <= max;
+}
+
+static bool IsValidPassword2(string definition)
+{
+    var parts = definition.Split(' ');
+    var limits = parts[0].Split('-');
+    var pos1 = int.Parse(limits[0]) - 1;
+    var pos2 = int.Parse(limits[1]) - 1;
+    var c = parts[1][0];
+
+    return (parts[2][pos1] == c) != (parts[2][pos2] == c);
+}
+
+Console.WriteLine($"Part 1: {lines.Count(IsValidPassword)}");
+Console.WriteLine($"Part 2: {lines.Count(IsValidPassword2)}");
--- src/2020/03/Program.cs
+++ src/2020/03/Program.cs
@@ -1,3 +1,35 @@
 ﻿using AdventOfCode.Common;
 
-var lines = Resources.GetResourceFileLines("input.txt");
+var map = Resources.GetResourceFileLines("input.txt").ParseAsJaggedArray(c => c == '#');
+
+static int CountTrees(bool[][] map, Coor change)
+{
+    var current = change;
+    var columns = map[0].Length;
+    var trees = 0;
+
+    while (current.Y < map.Length)
+    {
+        if (map[current.Y][current.X % columns])
+        {
+            trees++;
+        }
+
+        current += change;
+    }
+
+    return trees;
+}
+
+Console.WriteLine($"Part 1: {CountTrees(map, new Coor(1, 3))}");
+
+var slopes = new[]
+{
+    new Coor(1, 1),
+    new Coor(1, 3),
+    new Coor(1, 5),
+    new Coor(1, 7),
+    new Coor(2, 1),
+};
+
+Console.WriteLine($"Part 2: {slopes.Select(coor => (long)CountTrees(map, coor)).Aggregate((t, acc) => t * acc)}");
