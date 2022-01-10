fe87cc129e3a97048a66910ed2ee2e7a1db7a07c
Premek Vysoky <premek.vysoky@microsoft.com>
2022-01-10 18:25:29 +0100

    2020 Day 5

--- src/2020/05/Program.cs
+++ src/2020/05/Program.cs
@@ -1,3 +1,19 @@
 ﻿using AdventOfCode.Common;
 
-var lines = Resources.GetResourceFileLines("input.txt");
+var seats = Resources.GetResourceFile("input.txt")
+    .Replace("F", "0")
+    .Replace("B", "1")
+    .Replace("R", "1")
+    .Replace("L", "0")
+    .SplitBy(Environment.NewLine)
+    .Select(number => (Row: Convert.ToInt32(number.Substring(0, 7), 2), Column: Convert.ToInt32(number.Substring(7), 2)))
+    .Select(s => (Row: s.Row, Column: s.Column, Id: s.Row * 8 + s.Column))
+    .OrderBy(s => s.Id)
+    .ToList();
+
+Console.WriteLine($"Part 1: {seats.Last().Id}");
+
+var allIds = Enumerable.Range(seats.First().Id, seats.Last().Id - seats.First().Id);
+var missingIds = allIds.Except(seats.Select(s => s.Id));
+
+Console.WriteLine($"Part 2: {missingIds.First(id => seats.Any(s => s.Id == id - 1) && seats.Any(s => s.Id == id + 1))}");
