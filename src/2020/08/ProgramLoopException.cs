a65ba891cdba82954f6b921588593799f98eeef5
Premek Vysoky <premek.vysoky@microsoft.com>
2022-01-11 12:01:12 +0100

    2020 Day 8

---
+++ src/2020/08/ProgramLoopException.cs
@@ -0,0 +1,11 @@
+﻿namespace AdventOfCode._2020_08;
+
+class ProgramLoopException : Exception
+{
+    public int AccumulatorValue { get; }
+
+    public ProgramLoopException(int accumulatorValue)
+    {
+        AccumulatorValue = accumulatorValue;
+    }
+}
