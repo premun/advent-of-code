0b1391d701138e258b81535c981d1d0c3dc85828
Premek Vysoky <premek.vysoky@microsoft.com>
2022-01-11 12:01:12 +0100

    2020 Day 8

---
+++ src/2020/08/Operation.cs
@@ -0,0 +1,8 @@
+﻿namespace AdventOfCode._2020_08;
+
+enum Operation
+{
+    Nop,
+    Acc,
+    Jmp,
+}
--- src/2020/08/Program.cs
+++ src/2020/08/Program.cs
@@ -1,3 +1,92 @@
-﻿using AdventOfCode.Common;
+﻿using AdventOfCode._2020_08;
+using AdventOfCode.Common;
 
-var lines = Resources.GetResourceFileLines("input.txt");
+(Operation Operation, int Argument)[] instructions = Resources.GetResourceFileLines("input.txt")
+    .Select(line =>
+    {
+        var parts = line.Split(' ');
+        var value = int.Parse(parts[1].Replace("+", string.Empty));
+        var kind = parts[0] switch
+        {
+            "nop" => Operation.Nop,
+            "acc" => Operation.Acc,
+            "jmp" => Operation.Jmp,
+            _ => throw new NotImplementedException()
+        };
+
+        return (kind, value);
+    })
+    .ToArray();
+
+static int RunProgram((Operation Operation, int Argument)[] instructions)
+{
+    int pc = 0;
+    int acc = 0;
+    var runInstructions = new bool[instructions.Length];
+
+    while (pc < instructions.Length)
+    {
+        if (runInstructions[pc])
+        {
+            throw new ProgramLoopException(acc);
+        }
+
+        runInstructions[pc] = true;
+
+        switch (instructions[pc].Operation)
+        {
+            case Operation.Nop:
+                pc++;
+                break;
+
+            case Operation.Acc:
+                acc += instructions[pc].Argument;
+                pc++;
+                break;
+
+            case Operation.Jmp:
+                pc += instructions[pc].Argument;
+                break;
+        }
+    }
+
+    return acc;
+}
+
+try
+{
+    int acc = RunProgram(instructions);
+}
+catch (ProgramLoopException e)
+{
+    Console.WriteLine($"Part 1: {e.AccumulatorValue}");
+}
+
+for (int i = 0; i < instructions.Length; i++)
+{
+    var tmp = instructions[i];
+
+    switch (instructions[i].Operation)
+    {
+        case Operation.Nop:
+            instructions[i] = (Operation.Jmp, tmp.Argument);
+            break;
+
+        case Operation.Acc:
+            continue;
+
+        case Operation.Jmp:
+            instructions[i] = (Operation.Nop, tmp.Argument);
+            break;
+    }
+
+    try
+    {
+        Console.WriteLine($"Part 2: {RunProgram(instructions)}");
+    }
+    catch (ProgramLoopException)
+    {
+    }
+
+    instructions[i] = tmp;
+}
