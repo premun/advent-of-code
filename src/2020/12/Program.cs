4dede6175d6c7a68817e3f9d3e8f85c51c58f61f
Premek Vysoky <premek.vysoky@microsoft.com>
2022-01-15 11:47:58 +0100

    2020 Day 12

--- src/2020/12/Program.cs
+++ src/2020/12/Program.cs
@@ -1,3 +1,86 @@
-﻿using AdventOfCode.Common;
+﻿using System.Text.RegularExpressions;
+using AdventOfCode.Common;
 
 var lines = Resources.GetResourceFileLines("input.txt");
+
+static void Simulate(string[] input, Dictionary<char, Action<int>> actions)
+{
+    var regex = new Regex(@"^(?<dir>[NSWELRF])(?<value>\d+)$");
+
+    foreach (var line in input)
+    {
+        var match = regex.Match(line);
+        if (!match.Success)
+        {
+            throw new Exception("??? " + line);
+        }
+
+        var direction = match.Groups["dir"].ValueSpan[0];
+        var value = match.Groups["value"].ValueSpan;
+
+        actions[direction](int.Parse(value));
+    }
+}
+
+var direction = 0;
+var x = 0;
+var y = 0;
+
+var instructions = new Dictionary<char, Action<int>>
+{
+    { 'N', d => y -= d },
+    { 'S', d => y += d },
+    { 'W', d => x -= d },
+    { 'E', d => x += d },
+    { 'R', d => direction = (direction + d) % 360 },
+    { 'L', d => direction = (direction - d + 360) % 360 },
+    { 'F', d => (x, y) = direction switch
+        {
+            0 =>   (x + d, y),
+            90 =>  (x, y + d),
+            180 => (x - d, y),
+            270 => (x, y - d),
+            _ => throw new Exception("Angle not %90")
+        }
+    },
+};
+
+Simulate(lines, instructions);
+
+Console.WriteLine($"Part 1: {Math.Abs(x) + Math.Abs(y)}");
+
+x = 0;
+y = 0;
+var wx = 10;
+var wy = 1;
+
+instructions = new Dictionary<char, Action<int>>
+{
+    { 'N', d => wy += d },
+    { 'S', d => wy -= d },
+    { 'W', d => wx -= d },
+    { 'E', d => wx += d },
+    { 'R', d => (wx, wy) = d switch
+        {
+            0 =>   (wx, wy),
+            90 =>  (wy, -wx),
+            180 => (-wx, -wy),
+            270 => (-wy, wx),
+            _ => throw new Exception("Angle not %90")
+        }
+    },
+    { 'L', d => (wx, wy) = d switch
+        {
+            0 =>   (wx, wy),
+            90 =>  (-wy, wx),
+            180 => (-wx, -wy),
+            270 => (wy, -wx),
+            _ => throw new Exception("Angle not %90")
+        }
+    },
+    { 'F', d => (x, y) = (x + wx * d, y - wy * d) },
+};
+
+Simulate(lines, instructions);
+
+Console.WriteLine($"Part 2: {Math.Abs(x) + Math.Abs(y)}");
