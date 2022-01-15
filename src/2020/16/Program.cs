01a1b3e265b760654723b440f449798d6bb69448
Premek Vysoky <premek.vysoky@microsoft.com>
2022-01-15 20:35:01 +0100

    2020 Day 16 Part 1

--- src/2020/16/Program.cs
+++ src/2020/16/Program.cs
@@ -1,3 +1,36 @@
 ﻿using AdventOfCode.Common;
 
 var lines = Resources.GetResourceFileLines("input.txt");
+
+var fields = lines
+    .TakeWhile(l => l != "your ticket:")
+    .Select(line =>
+    {
+        var parts = line.Split(':');
+        var intervals = parts[1].Trim().Split(" or ");
+
+        return (Name: parts[0], Intervals: intervals.Select(i =>
+        {
+            var numbers = i.Split('-').Select(int.Parse);
+            return (Min: numbers.ElementAt(0), Max: numbers.ElementAt(1));
+        }).ToArray());
+    })
+    .ToArray();
+
+var myTicket = lines
+    .SkipWhile(l => l != "your ticket:")
+    .ElementAt(1)
+    .SplitToNumbers();
+
+var nearbyTickets = lines
+    .SkipWhile(l => l != "nearby tickets:")
+    .Skip(1)
+    .Select(l => l.SplitToNumbers())
+    .ToArray();
+
+var invalidRate = nearbyTickets
+    .SelectMany(n => n)
+    .Where(n => fields.All(f => f.Intervals.All(i => n < i.Min || n > i.Max)))
+    .Sum();
+
+Console.WriteLine($"Part 1: {invalidRate}");
