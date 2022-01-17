495aac57a6d797ea2c006c3576cf1f315a8614a2
Premek Vysoky <premek.vysoky@microsoft.com>
2022-01-17 10:33:44 +0100

    2020 Day 13 Part 1

--- src/2020/13/Program.cs
+++ src/2020/13/Program.cs
@@ -1,3 +1,20 @@
 ﻿using AdventOfCode.Common;
 
 var lines = Resources.GetResourceFileLines("input.txt");
+
+var arrival = int.Parse(lines.First());
+var buses = lines.Last()
+    .SplitBy(",")
+    .Where(b => b != "x")
+    .Select(int.Parse)
+    .ToList();
+
+var wait = -1;
+int bus = 0;
+do
+{
+    ++wait;
+    bus = buses.FirstOrDefault(b => (wait + arrival) % b == 0);
+} while (bus == 0);
+
+Console.WriteLine($"Part 1: {bus * wait}");
