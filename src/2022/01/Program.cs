7af3e4b02cdb86aa0067ddc748fb0708b57d8633
Premek Vysoky <premek.vysoky@microsoft.com>
2022-12-01 10:38:13 +0100

    2022 Day 1

--- src/2022/01/Program.cs
+++ src/2022/01/Program.cs
@@ -1,3 +1,24 @@
 ﻿using AdventOfCode.Common;
 
-var lines = Resources.GetResourceFileLines("input.txt");
+var lines = Resources.GetResourceFile("input.txt").Split(Environment.NewLine);
+
+var elfs = new List<List<int>>
+{
+    new()
+};
+
+foreach (var line in lines)
+{
+    if (string.IsNullOrEmpty(line))
+    {
+        elfs.Add(new());
+        continue;
+    }
+
+    elfs[^1].Add(int.Parse(line));
+}
+
+var orderedElfs = elfs.Select(elf => elf.Sum()).OrderDescending().ToList();
+
+Console.WriteLine($"Part 1: {orderedElfs.First()}");
+Console.WriteLine($"Part 2: {orderedElfs.Take(3).Sum()}");
