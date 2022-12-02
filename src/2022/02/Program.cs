6a68e56a84460afe60219930afdb8227c4d03bc8
Premek Vysoky <premek.vysoky@microsoft.com>
2022-12-02 10:25:39 +0100

    2022 Day 2

--- src/2022/02/Program.cs
+++ src/2022/02/Program.cs
@@ -1,3 +1,48 @@
 ﻿using AdventOfCode.Common;
 
-var lines = Resources.GetResourceFileLines("input.txt");
+var rounds = Resources.GetResourceFileLines("input.txt").Select(l => (l[0], l[2])).ToList();
+
+static int ChoiceScore(char choice) => choice < 'D'
+    ? choice - 'A' + 1
+    : choice - 'X' + 1;
+
+var score1 = rounds.Sum(c =>
+{
+    (char c1, char c2) = c;
+    var myScore = ChoiceScore(c2);
+
+    if (ChoiceScore(c1) == myScore)
+    {
+        return myScore + 3;
+    }
+
+    return myScore + (c1, c2) switch
+    {
+        ('A', 'Y') => 6,
+        ('A', 'Z') => 0,
+        ('B', 'X') => 0,
+        ('B', 'Z') => 6,
+        ('C', 'X') => 6,
+        /*('C', 'Y')*/_ => 0, 
+    };
+});
+
+var score2 = rounds.Sum(c => c.Item2 switch
+{
+    'X' => 0 + c.Item1 switch
+    {
+        'A' => 3,
+        'B' => 1,
+        _ => 2
+    },
+    'Y' => 3 + ChoiceScore(c.Item1),
+    _ => 6 + c.Item1 switch
+    {
+        'A' => 2,
+        'B' => 3,
+        _ => 1
+    },
+});
+
+Console.WriteLine($"Part 1: {score1}");
+Console.WriteLine($"Part 2: {score2}");
