993eb2929e4a5900841e3d2a0a3aaa047276e1bb
Premek Vysoky <premek.vysoky@microsoft.com>
2022-12-06 18:50:25 +0100

    2022 Day 6

--- src/2022/06/Program.cs
+++ src/2022/06/Program.cs
@@ -1,3 +1,10 @@
 ﻿using AdventOfCode.Common;
 
-var lines = Resources.GetInputFileLines();
+var signal = Resources.GetInputFileLines().Single();
+
+int GetMarker(int length) => Enumerable
+    .Range(length, signal.Length - length)
+    .First(index => signal.Substring(index - length, length).Distinct().Count() == length);
+
+Console.WriteLine($"Part 1: {GetMarker(4)}");
+Console.WriteLine($"Part 2: {GetMarker(14)}");
