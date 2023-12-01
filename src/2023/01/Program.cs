44a793259faba05fb2b38ae57483b9065ca887ef
Premek Vysoky <premek.vysoky@microsoft.com>
2023-12-01 10:05:34 +0100

    2023 Day 1

--- src/2023/01/Program.cs
+++ src/2023/01/Program.cs
@@ -1,3 +1,34 @@
 ﻿using AdventOfCode.Common;
 
 var lines = Resources.GetInputFileLines();
+
+Console.WriteLine($"Part 1: {Part1(lines)}");
+Console.WriteLine($"Part 2: {Part1(lines.Select(ReplaceDigits))}");
+
+static int Part1(IEnumerable<string> lines) => lines
+    .Select(l => l.Where(char.IsDigit))
+    .Select(digits => 10 * digits.First() + digits.Last())
+    .Sum();
+
+static string ReplaceDigits(string line)
+{
+    string[] digits = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+
+    for (int index = 0; index < line.Length; index++)
+    {
+        for (int d = 0; d < digits.Length; d++)
+        {
+            if (line[index..].StartsWith(digits[d]))
+            {
+                line =
+                    line.Substring(0, index + 1)
+                    + d
+                    + (index < line.Length - 1 ? line.Substring(index + 1) : null);
+
+                index += digits[d].Length - 1;
+            }
+        }
+    }
+
+    return line;
+}
