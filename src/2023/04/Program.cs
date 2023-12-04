1e01d9d7135846b775efd7dcb0d492185b0a2061
Premek Vysoky <premek.vysoky@microsoft.com>
2023-12-04 10:50:29 +0100

    2023 Day 4

--- src/2023/04/Program.cs
+++ src/2023/04/Program.cs
@@ -1,3 +1,41 @@
-﻿using AdventOfCode.Common;
+﻿using System.Text.RegularExpressions;
+using AdventOfCode.Common;
 
-var lines = Resources.GetInputFileLines();
+IReadOnlyCollection<Card> cards = Resources.GetInputFileLines()
+    .Select(Card.Parse)
+    .ToArray();
+
+var scores = cards
+    .Where(card => card.MatchingNumbers > 0)
+    .Select(card => Math.Pow(2, card.MatchingNumbers - 1));
+
+var wonCards = cards.ToDictionary(c => c.Id, c => 1L);
+for (int i = 0; i < cards.Count; i++)
+{
+    var card = cards.ElementAt(i);
+    for (int j = 1; j <= card.MatchingNumbers && j + card.Id <= cards.Count; j++)
+    {
+        wonCards[card.Id + j] += wonCards[card.Id];
+    }
+}
+
+Console.WriteLine($"Part 1: {scores.Sum()}");
+Console.WriteLine($"Part 2: {wonCards.Values.Sum()}");
+
+file record Card(int Id, int MatchingNumbers)
+{
+    private static readonly Regex ParseRegex = new(@"\d+");
+
+    public static Card Parse(string definition)
+    {
+        var digits = ParseRegex.Matches(definition)
+            .Select(m => int.Parse(m.Value))
+            .ToArray();
+
+        const int numOfNumbers = 10;
+        var winningNumbers = digits.Skip(1).Take(numOfNumbers);
+        var numbers = digits.Skip(numOfNumbers + 1);
+
+        return new Card(digits.First(), winningNumbers.Intersect(numbers).Count());
+    }
+}
