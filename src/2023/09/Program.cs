ae1f6190a5f5edc82cd8316812423ec38839b031
Premek Vysoky <premek.vysoky@microsoft.com>
2023-12-13 11:59:20 +0100

    2023 Day 9

--- src/2023/09/Program.cs
+++ src/2023/09/Program.cs
@@ -1,3 +1,48 @@
 ﻿using AdventOfCode.Common;
 
-var lines = Resources.GetInputFileLines();
+var extrapolatedNumbers = Resources.GetInputFileLines()
+    .Select(Resources.ParseLongNumbersOut)
+    .Select(Extrapolate)
+    .Aggregate((Start: 0L, End: 0L), (acc, v) => (acc.Start + v.Start, acc.End + v.End));
+
+Console.WriteLine($"Part 1: {extrapolatedNumbers.End}");
+Console.WriteLine($"Part 2: {extrapolatedNumbers.Start}");
+
+static (long Start, long End) Extrapolate(List<long> numbers)
+{
+    var sequences = new List<List<long>>(numbers.Count - 1)
+    {
+        numbers
+    };
+
+    bool allZeros = false;
+    do
+    {
+        allZeros = true;
+        var last = sequences.Last();
+        if (last.Count == 1)
+        {
+            break;
+        }
+
+        var bottom = new List<long>(last.Count - 1);
+        for (int i = 1; i < last.Count; i++)
+        {
+            var n = last[i] - last[i - 1];
+            bottom.Add(n);
+            allZeros &= n == 0;
+        }
+
+        sequences.Add(bottom);
+    } while (!allZeros);
+
+    var start = sequences.Last().First();
+    var end = sequences.Last().Last();
+    for (int i = sequences.Count - 1; i >= 0; --i)
+    {
+        end = sequences[i].Last() + end;
+        start = sequences[i].First() - start;
+    }
+
+    return (start, end);
+}
