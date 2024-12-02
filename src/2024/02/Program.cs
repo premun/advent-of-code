ae5b40b6b52cddf121540736c966ecd8b726da52
Premek Vysoky <premek.vysoky@microsoft.com>
2024-12-02 13:40:19 +0100

    2024 Day 2

--- src/2024/02/Program.cs
+++ src/2024/02/Program.cs
@@ -1,3 +1,38 @@
 ﻿using AdventOfCode.Common;
 
-var lines = Resources.GetResourceFileLines("input.txt");
+var levels = Resources.GetInputFileLines("input.txt")
+    .Select(line => line.SplitToNumbers(" "))
+    .ToList();
+
+var safeLevels = levels.Where(IsSafe);
+var almostSafeLevels = levels.Where(IsAlmostSafe);
+
+Console.WriteLine($"Part 1: {safeLevels.Count()}");
+Console.WriteLine($"Part 2: {almostSafeLevels.Count()}");
+
+static bool IsSafe(IList<int> level)
+{
+    var pairs = level.Take(level.Count - 1).Zip(level.Skip(1)).ToList();
+    return pairs.Select(p => Math.Abs(p.First - p.Second)).All(p => p >= 1 && p <= 3)
+        && (pairs.All(p => p.First > p.Second)
+         || pairs.All(p => p.First < p.Second));
+}
+
+static bool IsAlmostSafe(IList<int> level)
+{
+    if (IsSafe(level))
+    {
+        return true;
+    }
+
+    for (int i = 0; i < level.Count; i++)
+    {
+        var newLevel = level.Take(i).Concat(level.Skip(i + 1)).ToList();
+        if (IsSafe(newLevel))
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
