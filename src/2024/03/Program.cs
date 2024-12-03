dd2c7719a0899b2b57b393f01ac1d63fb5563bb5
Premek Vysoky <premek.vysoky@microsoft.com>
2024-12-03 09:52:51 +0100

    2024 Day 3

--- src/2024/03/Program.cs
+++ src/2024/03/Program.cs
@@ -1,6 +1,37 @@
-﻿using AdventOfCode.Common;
+﻿using System.Text.RegularExpressions;
+using AdventOfCode.Common;
 
-var lines = Resources.GetInputFileLines("input.txt");
+var content = Resources.GetInputFileContent("input.txt");
+var mulRegex = new Regex(@"(?<instruction>mul\((?<number1>\d+),(?<number2>\d+)\)|do\(\)|don't\(\))");
 
-Console.WriteLine($"Part 1: {""}");
-Console.WriteLine($"Part 2: {""}");
+var disabled = false;
+uint sum = 0;
+uint enabledOnlySum = 0;
+foreach (Match match in mulRegex.Matches(content))
+{
+    var instruction = match.Groups["instruction"].Value;
+
+    switch (instruction)
+    {
+        case "do()":
+            disabled = false;
+            continue;
+        case "don't()":
+            disabled = true;
+            continue;
+        default:
+            var number1 = uint.Parse(match.Groups["number1"].Value);
+            var number2 = uint.Parse(match.Groups["number2"].Value);
+            var mul = number1 * number2;
+            sum += mul;
+            if (!disabled)
+            {
+                enabledOnlySum += mul;
+            }
+
+            continue;
+    }
+}
+
+Console.WriteLine($"Part 1: {sum}");
+Console.WriteLine($"Part 2: {enabledOnlySum}");
