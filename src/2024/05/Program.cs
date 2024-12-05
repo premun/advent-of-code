03a689325841d54b1e8a49554c941d3dfbaf552c
Premek Vysoky <premek.vysoky@microsoft.com>
2024-12-06 00:03:05 +0100

    2024 Day 5

--- src/2024/05/Program.cs
+++ src/2024/05/Program.cs
@@ -2,5 +2,69 @@
 
 var lines = Resources.GetInputFileLines();
 
-Console.WriteLine($"Part 1: {""}");
-Console.WriteLine($"Part 2: {""}");
+var rules = lines
+    .TakeWhile(l => l.Contains('|'))
+    .Select(line => line.SplitToNumbers("|"))
+    .Select(numbers => (First: numbers[0], Second: numbers[1]));
+
+var updates = lines
+    .SkipWhile(l => l.Contains('|'))
+    .Select(line => line.SplitToNumbers());
+
+var dependentOn = new Dictionary<int, HashSet<int>>();
+foreach (var (former, latter) in rules)
+{
+    if (!dependentOn.TryGetValue(latter, out HashSet<int>? deps))
+    {
+        deps = [];
+        dependentOn[latter] = deps;
+    }
+
+    deps.Add(former);
+}
+
+Console.WriteLine($"Part 1: {GetMiddlePage(false, dependentOn, updates)}");
+Console.WriteLine($"Part 2: {GetMiddlePage(true, dependentOn, updates)}");
+
+static int GetMiddlePage(bool takeIncorrectlyReordered, Dictionary<int, HashSet<int>> rules, IEnumerable<int[]> updates)
+{
+    var result = 0;
+    foreach (var update in updates)
+    {
+        var isCorrect = true;
+        for (int i = 0; i < update.Length; i++)
+        {
+        Again:
+            var shouldBeEarlier = rules[update[i]];
+            for (int j = i + 1; j < update.Length; ++j)
+            {
+                if (shouldBeEarlier.Contains(update[j]))
+                {
+                    isCorrect = false;
+                    if (takeIncorrectlyReordered)
+                    {
+                        (update[i], update[j]) = (update[j], update[i]);
+                        i = 0;
+                        goto Again;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+
+            if (!takeIncorrectlyReordered && !isCorrect)
+            {
+                break;
+            }
+        }
+
+        if (takeIncorrectlyReordered != isCorrect)
+        {
+            result += update[update.Length / 2];
+        }
+    }
+
+    return result;
+}
