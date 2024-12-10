8d2e78d1f03b06e1f6344748e6320775b7496b21
Premek Vysoky <premek.vysoky@microsoft.com>
2024-12-10 23:13:40 +0100

    Simplify everything

--- src/2024/09/Program.cs
+++ src/2024/09/Program.cs
@@ -4,68 +4,53 @@ int[] diskMap = Resources.GetInputFileLines().First()
     .Select(c => c - '0')
     .ToArray();
 
-var forwardStream = new BlockStream(diskMap);
-var backwardStream = new ReverseBlockStream(diskMap);
-
-ulong checksum = 0UL;
-var next = forwardStream.GetNext()!;
-var last = backwardStream.GetNext()!;
-int position = 0;
-
 var debug = true;
 
-while (forwardStream < backwardStream)
+var forward = new BlockStream(diskMap);
+var backward = new BlockStream(diskMap.Reverse());
+var blockCount = diskMap.Length;
+var fileBlockCount = blockCount / 2;
+var position = 0;
+
+ulong checksum = 0UL;
+
+do
 {
-    while (next is FileBlock)
+    if (forward.Current is FileBlock first)
     {
-        MoveBlock(next.Id);
-        next = forwardStream.GetNext();
-
-        if (forwardStream > backwardStream)
-            break;
+        StoreBlock(first.FileId);
+        forward.MoveNext();
+        continue;
     }
 
-    while (last is FreeBlock)
+    if (backward.Current is not FileBlock last)
     {
-        last = backwardStream.GetNext();
-
-        if (forwardStream > backwardStream)
-            break;
+        backward.MoveNext();
+        continue;
     }
 
-    MoveBlock(last.Id);
-
-    next = forwardStream.GetNext();
-    if (forwardStream > backwardStream)
-        break;
-    last = backwardStream.GetNext();
-
-    if (forwardStream > backwardStream)
-        break;
-}
+    StoreBlock(fileBlockCount - last.FileId);
+    forward.MoveNext();
+    backward.MoveNext();
+} while (forward.Current.BlockId < blockCount - backward.Current.BlockId - 1);
 
 if (debug) Console.Write('.');
 
-// We ended up somewhere in a single file block, we need to process it
-//if (last is FileBlock)
-//{
-//    while (forwardStream != backwardStream)
-//    {
-//        MoveBlock(last.Id);
-//        last = backwardStream.GetNext();
-//    }
-//}
+while (backward.Current is FileBlock f && f.Bit < diskMap[forward.Current.BlockId] - forward.Current.Bit)
+{
+    StoreBlock(fileBlockCount - f.FileId);
+    backward.MoveNext();
+}
 
 Console.WriteLine();
-
 Console.WriteLine($"Part 1: {checksum}");
 Console.WriteLine($"Part 2: {""}");
 
-void MoveBlock(int blockId)
+void StoreBlock(int fileId)
 {
-    checksum += (ulong)(blockId * position);
+    checksum += (ulong)(fileId * position);
     position++;
-    if (debug) Console.Write((char)(blockId + '0'));
+    if (debug) Console.Write((char)(fileId + '0'));
 }
 
 file class BlockStream
@@ -79,23 +64,24 @@ file class BlockStream
     public BlockStream(IEnumerable<int> diskMap)
     {
         _diskMap = diskMap.GetEnumerator();
-        _blockId = 0;
-        _diskMap.MoveNext();
         _bit = 0;
+        _diskMap.MoveNext();
     }
 
-    protected virtual int Bit => _bit - 1;
-    protected virtual int BlockId => _blockId;
-    protected virtual int FileId => _fileId;
+    public Block Current => _currentBlockIsFile
+        ? new FileBlock(_fileId, _blockId, _bit)
+        : new FreeBlock(_blockId, _bit);
 
-    public Block GetNext()
+    public bool MoveNext()
     {
         _bit++;
+
+        // Moving onto a new file block
         while (_bit == _diskMap.Current)
         {
             if (!_diskMap.MoveNext())
             {
-                return new FreeBlock(FileId, int.MaxValue);
+                return false;
             }
 
             _bit = 0;
@@ -107,45 +93,10 @@ file class BlockStream
             _blockId++;
         }
 
-        return _currentBlockIsFile
-            ? new FileBlock(FileId, _diskMap.Current)
-            : new FreeBlock(_blockId, _diskMap.Current);
-    }
-
-    public static bool operator <(BlockStream first, BlockStream second)
-    {
-        if (first.BlockId < second.BlockId) return true;
-        if (first.BlockId == second.BlockId) return first.Bit < second.Bit;
-        return false;
-    }
-
-    public static bool operator >(BlockStream first, BlockStream second)
-    {
-        return second < first;
-    }
-
-    public static bool operator ==(BlockStream first, BlockStream second)
-    {
-        return first.BlockId == second.BlockId && first.Bit == second.Bit;
-    }
-
-    public static bool operator !=(BlockStream first, BlockStream second)
-    {
-        return first.BlockId != second.BlockId || first.Bit == second.Bit;
+        return true;
     }
 }
 
-file class ReverseBlockStream(int[] diskMap)
-    : BlockStream(diskMap.Reverse())
-{
-    private readonly int _maxFileId = diskMap.Length / 2;
-    private readonly int _maxBlockId = diskMap.Length - 1;
-
-    protected override int FileId => _maxFileId - _fileId;
-    protected override int Bit => _diskMap.Current - _bit - 1;
-    protected override int BlockId => _maxBlockId - _blockId;
-}
-
-abstract file record Block(int Id, int Size);
-file record FileBlock(int Id, int Size) : Block(Id, Size);
-file record FreeBlock(int Id, int Size) : Block(Id, Size);
+abstract file record Block(int BlockId, int Bit);
+file record FileBlock(int FileId, int BlockId, int Bit) : Block(BlockId, Bit);
+file record FreeBlock(int BlockId, int Bit) : Block(BlockId, Bit);
