1f23bc6c059052fa4e83473cf2636f8c5483fa8e
Premek Vysoky <premek.vysoky@microsoft.com>
2024-12-10 09:07:35 +0100

    2024 Day 10

--- src/2024/10/Program.cs
+++ src/2024/10/Program.cs
@@ -1,6 +1,39 @@
 ﻿using AdventOfCode.Common;
+using Coor = AdventOfCode.Common.Coor<int>;
 
-var lines = Resources.GetInputFileLines();
+var map = Resources.GetInputFileLines()
+    .ParseAsArray(c => c - '0');
 
-Console.WriteLine($"Part 1: {""}");
-Console.WriteLine($"Part 2: {""}");
+var starts = map.AllCoordinates()
+    .Where(c => map.Get(c) == 0);
+
+Console.WriteLine($"Part 1: {starts.Select(s => GetScore(s, false)).Sum()}");
+Console.WriteLine($"Part 2: {starts.Select(s => GetScore(s, true)).Sum()}");
+
+int GetScore(Coor position, bool distinctPaths)
+{
+    return distinctPaths
+        ? GetReachableNines(position).Count
+        : GetReachableNines(position).Distinct().Count();
+}
+
+List<Coor> GetReachableNines(Coor position, List<Coor>? reachableNines = null)
+{
+    reachableNines ??= [];
+    var target = map.Get(position) + 1;
+    if (target == 10)
+    {
+        reachableNines.Add(position);
+        return reachableNines;
+    }
+
+    foreach (var n in position.GetFourWayNeighbours())
+    {
+        if (n.InBoundsOf(map) && map.Get(n) == target)
+        {
+            GetReachableNines(n, reachableNines);
+        }
+    }
+
+    return reachableNines;
+}
