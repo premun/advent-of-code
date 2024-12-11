9a9c0de762a3418d9a140caedcdff70444dcb9c3
Premek Vysoky <premek.vysoky@microsoft.com>
2024-12-12 00:13:25 +0100

    Parallel 2024 Day 11

--- src/2024/11/Program.cs
+++ src/2024/11/Program.cs
@@ -1,108 +1,68 @@
-﻿using System.Collections;
+﻿using System.Collections.Concurrent;
 using AdventOfCode.Common;
 
-var values = Resources.GetInputFileLines()
+var stones = Resources.GetInputFileLines()
     .First()
-    .SplitToNumbers(" ");
+    .SplitToNumbers(" ")
+    .Select(v => new Stone((ulong)v, 0));
 
-var head = Initialize(values);
+var queue = new ConcurrentQueue<Stone>(stones);
 
-for (int i = 0; i < 75; ++i)
+var maxGenerations = 75;
+var total = 0UL;
+int i = 0;
+
+while (!queue.IsEmpty)
 {
-    Console.WriteLine(i);
+    Console.WriteLine(queue.First().Generation);
+    //Console.WriteLine(string.Join(", ", queue.Select(s => $"{s.Number},{s.Generation}")));
 
-    var stone = head;
-    while (stone != null)
+    Parallel.ForEach(queue, _ =>
     {
-        stone = stone.Blink();
-    }
+        if (!queue.TryDequeue(out Stone stone)) return;
+        var newGeneration = stone.Generation + 1;
 
-    //Console.WriteLine(string.Join(", ", head));
+        if (stone.Number == 0)
+        {
+            Enqueue(new Stone(1, newGeneration));
+            return;
+        }
+
+        var digits = (int)(Math.Log10(stone.Number) + 1);
+        if (digits % 2 != 0)
+        {
+            Enqueue(new Stone(stone.Number * 2024, newGeneration));
+            return;
+        }
+
+        var div = (ulong)Math.Pow(10, digits / 2);
+        var left = stone.Number / div;
+        var right = stone.Number - left * div;
+
+        Enqueue(new Stone(left, newGeneration));
+        Enqueue(new Stone(right, newGeneration));
+    });
 }
 
-Console.WriteLine($"Part 1: {head.Count()}");
+Console.WriteLine(total);
+
+void Enqueue(Stone stone)
+{
+    if (stone.Generation == maxGenerations)
+    {
+        lock (stones)
+        {
+            //Console.WriteLine(stone.Number);
+            Interlocked.Increment(ref total);
+        }
+    }
+    else
+    {
+        queue.Enqueue(stone);
+    }
+}
+
+Console.WriteLine($"Part 1: {total}");
 Console.WriteLine($"Part 2: {""}");
 
-static Stone Initialize(int[] values)
-{
-    var head = new Stone((ulong)values.First());
-    var previous = head;
-    for (int i = 1; i < values.Length; i++)
-    {
-        var newStone = new Stone((ulong)values[i], previous, null);
-        if (previous != null)
-            previous.Next = newStone;
-
-        previous = newStone;
-    }
-
-    return head;
-}
-
-file class Stone(ulong number, Stone? previous = null, Stone? next = null) : IEnumerable<ulong>
-{
-    public ulong Number { get; set; } = number;
-    public Stone? Previous { get; set; } = previous;
-    public Stone? Next { get; set; } = next;
-
-    public Stone? Blink()
-    {
-        if (Number == 0)
-        {
-            Number = 1;
-            return Next;
-        }
-
-        var digits = (int)(Math.Log10(Number) + 1);
-        if (digits % 2 == 0)
-        {
-            var div = (ulong)Math.Pow(10, digits / 2);
-            var left = Number / div;
-            var right = Number - left * div;
-
-            var newStone = new Stone(right, this, Next);
-
-            Number = left;
-            Next = newStone;
-
-            return newStone.Next;
-        }
-
-        Number *= 2024;
-        return Next;
-    }
-
-    public IEnumerator<ulong> GetEnumerator() => new StoneEnumerator(this);
-
-    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
-
-    private class StoneEnumerator(Stone head) : IEnumerator<ulong>
-    {
-        private readonly Stone _head = head;
-        private Stone? _current = null;
-
-        public ulong Current => _current!.Number;
-
-        object IEnumerator.Current => Current;
-
-        public void Dispose() {}
-        public bool MoveNext()
-        {
-            if (_current == null)
-            {
-                _current = _head;
-                return true;
-            }
-
-            if (_current.Next == null)
-            {
-                return false;
-            }
-
-            _current = _current.Next;
-            return true;
-        }
-
-        public void Reset() => _current = _head;
-    }
-}
+readonly file record struct Stone(ulong Number, int Generation);
