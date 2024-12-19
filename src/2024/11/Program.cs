746fbed769c1338ccd097187650a1333ae2a88ae
Premek Vysoky <premek.vysoky@microsoft.com>
2024-12-19 17:21:04 +0100

    Count different stones

--- src/2024/11/Program.cs
+++ src/2024/11/Program.cs
@@ -6,42 +6,55 @@ var stones = Resources.GetInputFileLines()
     .SplitToNumbers(" ")
     .Select(v => new Stone((ulong)v, 0));
 
-var queue = new ConcurrentQueue<Stone>(stones);
+var queue = new ConcurrentQueue<Stone>();
 
 var maxGenerations = 75;
 var total = 0UL;
-int i = 0;
 
-while (!queue.IsEmpty)
+var differentStones = new ConcurrentDictionary<ulong, bool>();
+
+foreach (var s in stones)
 {
-    Console.WriteLine(queue.First().Generation);
-    //Console.WriteLine(string.Join(", ", queue.Select(s => $"{s.Number},{s.Generation}")));
-
-    Parallel.ForEach(queue, _ =>
+    Console.WriteLine("Starting stone " + s.Number);
+    queue.Enqueue(s);
+    while (!queue.IsEmpty)
     {
-        if (!queue.TryDequeue(out Stone stone)) return;
-        var newGeneration = stone.Generation + 1;
+        Console.Clear();
+        Console.Write("[");
+        var gen = queue.First().Generation;
+        Console.Write(new string('|', gen));
+        Console.Write(new string(' ', maxGenerations - gen));
+        Console.WriteLine($"] {gen} / {maxGenerations} ({queue.Count})");
+        Console.WriteLine("Total: " + total);
+        Console.WriteLine("Different ones: " + differentStones.Keys.Count);
+        //Console.WriteLine(string.Join(", ", queue.Select(s => $"{s.Number},{s.Generation}")));
 
-        if (stone.Number == 0)
+        Parallel.ForEach(queue, _ =>
         {
-            Enqueue(new Stone(1, newGeneration));
-            return;
-        }
+            if (!queue.TryDequeue(out Stone stone)) return;
+            var newGeneration = stone.Generation + 1;
 
-        var digits = (int)(Math.Log10(stone.Number) + 1);
-        if (digits % 2 != 0)
-        {
-            Enqueue(new Stone(stone.Number * 2024, newGeneration));
-            return;
-        }
+            if (stone.Number == 0)
+            {
+                Enqueue(new Stone(1, newGeneration));
+                return;
+            }
 
-        var div = (ulong)Math.Pow(10, digits / 2);
-        var left = stone.Number / div;
-        var right = stone.Number - left * div;
+            var digits = (int)(Math.Log10(stone.Number) + 1);
+            if (digits % 2 != 0)
+            {
+                Enqueue(new Stone(stone.Number * 2024, newGeneration));
+                return;
+            }
 
-        Enqueue(new Stone(left, newGeneration));
-        Enqueue(new Stone(right, newGeneration));
-    });
+            var div = (ulong)Math.Pow(10, digits / 2);
+            var left = stone.Number / div;
+            var right = stone.Number - left * div;
+
+            Enqueue(new Stone(left, newGeneration));
+            Enqueue(new Stone(right, newGeneration));
+        });
+    }
 }
 
 Console.WriteLine(total);
@@ -59,6 +72,7 @@ void Enqueue(Stone stone)
     else
     {
         queue.Enqueue(stone);
+        differentStones.TryAdd(stone.Number, true);
     }
 }
 
